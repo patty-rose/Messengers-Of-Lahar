e52bdb3f308cb2c2248fd7e6a6e92bc7fde0bdfd
Patty Rose <oteropatty@gmail.com>
2022-12-22 19:53:43 -0800

    initial commit with basic Firestore files and dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { collection, addDoc, onSnapshot, updateDoc, doc, deleteDoc, query, orderBy } from "firebase/firestore";
+import db from './firebase.js'
+import { BrowserRouter as Router, Routes, Route, Link, Outlet } from 'react-router-dom';
 
-function App() {
+function App(){
+
+  //useState hooks:
+  const [mainPageList, setMainPageList] = useState([]);
+  const [selectedPage, setSelectedPage] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const unSubscribe = onSnapshot(
+      collection(db, 'pages'),
+      (collectionSnapshot) => {
+        const pages = [];
+        collectionSnapshot.forEach((doc) => {
+          pages.push({
+            pageText: doc.data().pageText,
+            backgroundImage: doc.data().backgroundImage,
+            pageNumber: doc.data().pageNumber,
+            id: doc.id,
+          });
+        });
+        setMainPageList(pages);
+      },
+      (error) => {
+        //add more
+      }
+    );
+    return () => unSubscribe();
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      mainPageList:{mainPageList}
+    </>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<MolTitlePage />} />
+    //     <Route path="/MolPages/:thisPageNumber" element={<MolPages mainPageList = {mainPageList}/>} />
+
+    //     <Route 
+    //       path="/admin" 
+    //       element={<PageControl />} >
+
+    //       <Route index element={<PageList mainPageList = {mainPageList} setMainPageList = {setMainPageList} selectedPage = {selectedPage} setSelectedPage = {setSelectedPage} />} />
+    //       <Route path='AddPage' element={<AddPage />} />
+    //       <Route path='details/:thisPageId' element={<PageDetail mainPageList = {mainPageList} />}/>
+    //       <Route path='editPage/:thisPageId' element={<EditPage mainPageList = {mainPageList} />}/>
+
+    //     </Route>
+
+    //     <Route path='*' element={<Error />} />
+        
+    //   </Routes>
+    // </Router>
   );
 }
 
