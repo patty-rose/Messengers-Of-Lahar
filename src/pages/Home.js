7559d21f34944a4e3b2b459f3cf7b0d820299666
Patty Rose <oteropatty@gmail.com>
2023-01-06 22:39:58 -0800

    WIP styling pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div class="container main">
+      <h1>Messengers of Lahar</h1>
+      <p>
+        Live your desires and reverse the shame thrust on you by those fearful
+        of exploration
+      </p>
+      <Link to="/" style={{ textDecoration: "none", color: "#4F5361" }}>
+        <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+          link left
+        </Typography>
+      </Link>
+      <Link to="/" style={{ textDecoration: "none", color: "#4F5361" }}>
+        <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+          link right
+        </Typography>
+      </Link>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
