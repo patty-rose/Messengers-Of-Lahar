095e260a248a555d270d6f267c8be48638770464
Patty Rose <oteropatty@gmail.com>
2023-01-10 21:45:12 -0800

    add functional navigation buttons to MolPages

--- src/pages/MolPage.js
+++ src/pages/MolPage.js
@@ -1,16 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link, useParams } from "react-router-dom";
-import { Typography } from "@mui/material";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
 import { red } from "@mui/material/colors";
+import candle from "../img/candle.png";
+import hand from "../img/hand.png";
 
 const MolPage = (props) => {
   const { listOfPages, onGetRandomPageId } = props;
   const { pageId } = useParams();
+  const navigate = useNavigate();
 
   const thisPage = listOfPages.find((page) => page.id === pageId);
 
   const randomPageId = onGetRandomPageId(listOfPages);
+
+  const handleNavigationClick = () => {
+    navigate(`/${randomPageId}`)
+    console.log("trying to navigate", randomPageId);
+  }
 
   const molPageStyle = {
     backgroundImage: `url('${thisPage.backgroundImage}')`,
@@ -24,23 +32,31 @@ const MolPage = (props) => {
       <p>{thisPage.pageText}</p>
       <p>{thisPage.backgroundImage}</p>
 
-      <Link
-        to={`/${randomPageId}`}
-        style={{ textDecoration: "none", color: "#4F5361" }}
-      >
-        <Typography variant="body2" sx={{ fontWeight: "bold" }}>
-          link left
-        </Typography>
-      </Link>
+      <Box
+        component="img"
+        sx={{
+          height: "30%",
+          position: "fixed",
+          bottom: 0,
+          left: "15%"
+        }}
+        alt="a black and white hand, palm facing viewer, holding up thumb, index, and middle finger, with pinky and ring tucked and pressing on palm."
+        src={hand}
+        onClick= {()=>{handleNavigationClick()}}
+      />
 
-      <Link
-        to={`/${randomPageId}`}
-        style={{ textDecoration: "none", color: "#4F5361" }}
-      >
-        <Typography variant="body2" sx={{ fontWeight: "bold" }}>
-          link right
-        </Typography>
-      </Link>
+      <Box
+        component="img"
+        sx={{
+          height: "30%",
+          position: "fixed",
+          bottom: 0,
+          right: "15%"
+        }}
+        alt="a black and white lit candle"
+        src={candle}
+        onClick= {()=>{handleNavigationClick()}}
+      />
     </div>
   );
 };
